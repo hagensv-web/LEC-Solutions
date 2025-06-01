73e73eb8cc015c3dd036adad2e42d61d2e2568ef
HagenSv <HagenSV22@gcc.edu>
2025-06-01 16:15:09 -0400

    Configure for github pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "/lec-solutions",
+  assetPrefix: "/lec-solutions/",
 };
 
 export default nextConfig;
