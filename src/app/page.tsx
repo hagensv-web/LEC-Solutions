7b9bc665b4c21e300786c39226116d78d12e6853
HagenSv <HagenSV22@gcc.edu>
2025-06-01 17:34:45 -0400

    Update image path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
       <p>LEC Solutions provides calibration gas to ensure your life-saving systems are functioning properly. We have partnered with one of the finest manufacturers in the industry to provide you with high-quality calibration gas that will meet your demands and not break your wallet. Our staff is well trained and understands the complexity of calibration gas. We offer next day service throughout the United States and have thousands of mixes to choose along with the best regulators in the industry.</p>
       </Col>
       <Col md={6}>
-      <Image src="/Group_Photo.png" alt="Calibration Gas" width={500} height={300} />
+      <Image src="/images/Group_Photo.png" alt="Calibration Gas" width={500} height={300} />
       </Col>
       </Row>
       </Container>
