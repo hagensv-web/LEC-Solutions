db00b9ed8774ebf5381ca042a4542fe66edc00f1
HagenSv <HagenSV22@gcc.edu>
2025-06-01 16:15:00 -0400

    Fix navbar link

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import styles from "./Navbar.module.css";
 
 
@@ -5,10 +6,10 @@ export default function Nav(){
     return (
         <div className={styles.navbar}>
             <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/products">Products</a></li>
-                <li><a href="/services">Services</a></li>
-                <li><a href="/contact">Contact</a></li>
+                <li><Link href="/">Home</Link></li>
+                <li><Link href="/products">Products</Link></li>
+                <li><Link href="/services">Services</Link></li>
+                <li><Link href="/contact">Contact</Link></li>
             </ul>
         </div>
     )
