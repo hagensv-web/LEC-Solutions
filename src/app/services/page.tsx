93443512820721308da4a921ba54a7fa0d4db2ca
HagenSv <HagenSV22@gcc.edu>
2025-06-01 19:13:15 -0400

    format services page

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,10 +1,14 @@
+import { Container } from "react-bootstrap"
+
 export default function Services() {
   return (
     <main>
+      <Container>
         <h1>Services</h1>
         <p>LEC Solitons, Inc offers calibration services for all major gas detection manufacturers. Your device will be cleaned and calibrated to the manufacturer specifications.</p>
         <p>Each device will be thoroughly cleaned, troubleshot, calibrated, and will recieve a calibration certificate.</p>
         <p>LEC Solutions, Inc is an authorized services center. We are able to take care of any service related issues that may arise and we are able to do this for half the price of what the other leading manufaturers charge. We are not here to nickle and dime you to death. We are committed to your safety</p>
+      </Container>
     </main>
   );
 }
