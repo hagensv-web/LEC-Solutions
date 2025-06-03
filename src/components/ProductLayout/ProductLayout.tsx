ad9f58b95df94d7ac392fd487fa03c65167d21f7
HagenSv <HagenSV22@gcc.edu>
2025-06-02 22:19:22 -0400

    Make page server component

---
+++ src/components/ProductLayout/ProductLayout.tsx
@@ -0,0 +1,29 @@
+import getProducts from "@/api/products";
+import { Product } from "@/types/product";
+import { useEffect, useState } from "react";
+import { Col, Row } from "react-bootstrap";
+import Listing from "../ProductListing/ProductListing";
+
+export default function ProductLayout(){
+    const [products, setProducts] = useState<Product[]>([]);
+
+    useEffect(() => {
+
+        async function fetchProducts() {
+            const products = await getProducts()
+            setProducts(products);
+        }
+        
+        fetchProducts()
+    }, []);
+
+    return (
+        <Row className="g-5">
+            {products.map((product) => (
+                <Col md={12} lg={6} xxl={4} key={product.id}>
+                    <Listing product={product} />
+                </Col>
+            ))}
+        </Row>
+    );
+}
