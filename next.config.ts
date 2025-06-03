d752fa8c84ff91ae53872b929ef9b7071e063fef
HagenSV <HagenSV22@gcc.edu>
2025-06-03 18:55:21 -0400

    Configure export

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
   basePath: "/lec-solutions",
   assetPrefix: "/lec-solutions/",
 };
