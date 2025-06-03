97631f8aa25aa478b3c4f397741f2af1c06a6132
HagenSV <HagenSV22@gcc.edu>
2025-06-03 19:36:51 -0400

    Fix next config for deploy

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  basePath: "/lec-solutions",
-  assetPrefix: "/lec-solutions/",
+  basePath: "/LEC-Solutions",
+  assetPrefix: "/LEC-Solutions/",
 };
 
 export default nextConfig;
