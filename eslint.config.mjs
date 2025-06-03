df326c35fca3a45ad91bfb4e2018bde372a4facc
HagenSV <HagenSV22@gcc.edu>
2025-06-03 19:06:44 -0400

    Configure eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,11 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals", "next/typescript", {
+    rules: {
+      "@next/next/no-img-element": "off", // Allow usage of <img> elements
+    },
+  }), 
 ];
 
 export default eslintConfig;
